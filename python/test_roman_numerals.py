677e46f77281b9dac1555a089ca018b5739829d9
Mateus <seanlilmateus@users.noreply.github.com>
2023-11-09 11:46:48 +0000

    running

--- python/test_roman_numerals.py
+++ python/test_roman_numerals.py
@@ -18,8 +18,8 @@ ROMANS = {
 }
 
 class RomanNumeralsTest(unittest.TestCase):
-    def test_arabic_converts_into_roman_I2VII(self):
-        for roman, arabic in  [("I", 1), ("VI", 6), ("VII", 7), ("XX", 20)]:
+    def test_arabic_converts_into_romans(self):
+        for roman, arabic in  [("I", 1), ("VI", 6), ("VII", 7), ("XX", 20), ("CII", 102), ("MMXXIII", 2023)]:
             actual = convert(arabic)
             self.assertEqual(actual, roman)
 
@@ -27,11 +27,15 @@ class RomanNumeralsTest(unittest.TestCase):
 def convert(arabic):
     return ROMANS.get(arabic, compose_roman2(arabic))
 
+def compose_roman2(num):
+    def encode(memo2, pair):
+        arabic, roman = pair
+        num2, memo = memo2
+        quotient, num2 = divmod(num2, arabic)
+        return (num2, memo + roman * quotient)
+    return reduce(encode, ROMANS.items(), (num, ""))[-1]
 
 def compose_roman(num):
-    return 'VI'
-
-def compose_roman2(num):
     roman_numerals = ''
     for value in sorted(ROMANS.keys(), reverse=True):
         while num >= value:
